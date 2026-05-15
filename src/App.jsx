91fa278d4363e27926b3c579f1d122e6a23f9ebe
Lubeth <lubethjimenez@yahoo.com>
2026-05-15 15:54:37 -0400

    Add CRUD: basic READ

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 
+import { useEffect, useState } from 'react'
+import { supabase } from './utils/supabase'
 import './App.css'
 
 function App() {
-  
+//READ from Database//
+    const [students, setStudents] = useState([]);
+
+//READ//
+async function fetchStudents() {
+   try {
+    const {data, error} = await supabase.from("Directory").select("*");
+    if(error){
+      console.log(error);
+    } else {
+      console.log(data);
+      setStudents(data);
+    }
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+   useEffect(()=>{
+    fetchStudents();
+   }, []);
 
   return (
     <>
-      <h1>Test</h1>
+      <h1>List</h1>
+        {/*READ fromDatabase*/}
+      <div>
+        {students.map((student) => (
+          <div key={student.id}>
+            <h2>{student.name}</h2>
+            <p>{student.major}</p>
+            <p>{student.year}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
 export default App
