c506a8b03c03e8e07d5ca1992aefef5a045f7d76
Lubeth <lubethjimenez@yahoo.com>
2026-05-15 16:02:52 -0400

    Add CRUD: basic CREATE

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
 //READ from Database//
     const [students, setStudents] = useState([]);
+//Form Add/CREATE State//
+    const [name, setName] = useState("");
+    const [major, setMajor] = useState("");
+    const [year, setYear] = useState("");
 
 //READ//
 async function fetchStudents() {
@@ -21,6 +25,29 @@ async function fetchStudents() {
       console.log(error);
    }
 }
+//Form Add/CREATE//
+async function addStudent(event) {
+   event.preventDefault();
+   try {
+     const {data, error} = await supabase.from("Directory").insert([{
+      name: name, major: major, year: year,
+     }]);
+     if(error){
+      console.log(error);
+     } else {
+      console.log("Student Added");
+      //Clear Form//
+      setName("");
+      setMajor("");
+      setYear("");
+      //Refresh List//
+      fetchStudents();
+     }
+   } catch (error) {
+      console,log(error);
+      console.log(addStudent);
+   }
+}
 
    useEffect(()=>{
     fetchStudents();
@@ -29,7 +56,29 @@ async function fetchStudents() {
   return (
     <>
       <h1>List</h1>
-        {/*READ fromDatabase*/}
+        {/*Form Add/CREATE*/}
+      <form onSubmit={addStudent}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Major"
+          value={major}
+          onChange={(event) => setMajor(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Year"
+          value={year}
+          onChange={(event) => setYear(event.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {/*READ fromDatabase*/}
       <div>
         {students.map((student) => (
           <div key={student.id}>
